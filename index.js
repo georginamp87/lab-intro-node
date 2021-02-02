960786e7cf8494ace982ac2e57811305730b48f5
Georgina Morales <georginamp87@gmail.com>
2021-02-02 19:17:00 +0100

    done and with bonuses wuhuu

--- index.js
+++ index.js
@@ -1,17 +1,67 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length++
+    this.items.sort((a,b) => {
+      return (a - b)
+    }) 
+      return this.items
+  }
 
-  get(pos) {}
+  get(pos) {
+      if (this.items[pos] !== undefined) {
+      return (this.items[pos])
+    } else {
+      throw new Error('OutOfBounds') 
+    }
+  }
+  
+  max() {
+    let maxNum = Math.max(...this.items)
+    if (this.length !== 0)
+    return maxNum
+    else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  max() {}
+  min() {
+    let minNum = Math.min(...this.items)
+    if (this.length !== 0)
+    return minNum
+    else {
+      throw new Error('EmptySortedList')
+    }}
 
-  min() {}
+  sum() {
+    let sum = 0
+    if (this.length !== 0) {
+    for (let i = 0 ; i < this.items.length ; i++) {
+      sum += this.items[i]
+    } return sum
+  } else {
+    return 0
+  }
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.length !== 0) {
+     let avgValue = 0
+         for (let i = 0 ; i < this.items.length ; i++) {
+         avgValue = this.sum()/this.length 
+      }
+        return avgValue
+      } 
+       else {
+        throw new Error('EmptySortedList')
+  }
+  
+  }
 }
 
 module.exports = SortedList;
